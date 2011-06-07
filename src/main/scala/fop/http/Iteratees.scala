46b677df6b07fac0d30d709f889281985c390137
eric thul <eric.thul@gmail.com>
2011-06-06 23:16:34 -0400

    reworked test case to http spec

--- src/main/scala/fop/http/Iteratees.scala
+++ src/main/scala/fop/http/Iteratees.scala
@@ -6,46 +6,18 @@ import scalaz._, scalaz.effects._, Scalaz._, IterV._
 object Iteratees {
   import Utils._
 
-  def getStatusLine: IterV[Chunk,String] = getBytesUntilSep("\n")
-
-  def getHeaders: IterV[Chunk,String] = getBytesUntilSep("\n\n")
-
-  def getPayload: IterV[Chunk,String] = {
-    def step(acc: StringBuilder)(s: Input[Chunk]): IterV[Chunk,String] = {
-      s(el = e => Cont(step(acc appendAll e.map(_.toChar)))
-      , empty = Cont(step(acc))
-      , eof = Done(acc toString, EOF[Chunk])
-      )
-    }
-    Cont(step(new StringBuilder))
-  }
-
-  def popEol: IterV[Chunk,Unit] = {
-    def step(s: Input[Chunk]): IterV[Chunk,Unit] = {
-      s(el = e => {
-          if (e.indexOf('\n'.toByte) == 0) Done((), El(e tail))
-          else Done((), s)
-        }
-      , empty = Cont(step)
-      , eof = Done((), EOF[Chunk])
-      )
-    }
-    Cont(step)
-  }
-
   def httpResponse: IterV[Chunk,(String,String,String)] = {
     for {
-      a <- getStatusLine
-      _ <- popEol
-      b <- getHeaders
-      _ <- popEol
-      _ <- popEol
-      c <- getPayload
+      // TODO: ethul, skip leading newlines
+      a <- line 
+      b <- linesUntilEmptyLine
+      _ <- newline
+      c <- lines
     } yield (a,b,c)
   }
 
   private object Utils {
-    def getBytesUntilSep: String => IterV[Chunk,String] = sep => {
+    def line: IterV[Chunk,String] = {
       def step(acc: StringBuilder)(s: Input[Chunk]): IterV[Chunk,String] = {
         s(el = e => {
             // we have to handle the case when part of the sep is already in the acc
@@ -53,11 +25,11 @@ object Iteratees {
             // then if the sep is in fact inside, we pull it out along with the chunk
             // part following the sep and pass it along as unprocessed
             val acc1 = acc.appendAll(e.map(_.toChar))
-            val i = acc1.indexOf(sep)
+            val i = acc1.indexOf("\n")
             if (i != -1) {
               val res = acc1.toString
-              val left = res.slice(0, i)
-              val right = res.slice(i, res.length)
+              val left = res.slice(0, i+1)
+              val right = if (i == res.length-1) "" else res.slice(i+1, res.length)
               Done(left, El(right.toArray.map(_.toByte)))
             }
             else Cont(step(acc1))
@@ -67,6 +39,53 @@ object Iteratees {
         )
       }
       Cont(step(new StringBuilder))
+    }
+
+    def lines: IterV[Chunk,String] = {
+      def step(acc: StringBuilder)(s: Input[Chunk]): IterV[Chunk,String] = {
+        s(el = e => Cont(step(acc appendAll e.map(_.toChar)))
+        , empty = Cont(step(acc))
+        , eof = Done(acc toString, EOF[Chunk])
+        )
+      }
+      Cont(step(new StringBuilder))
+    }
+
+    // TODO: ethul, this iteratee should reuse peek and readLine
+    def linesUntilEmptyLine: IterV[Chunk,String] = {
+      def step(acc: StringBuilder)(s: Input[Chunk]): IterV[Chunk,String] = {
+        s(el = e => {
+            if (acc.isEmpty && e.indexOf('\n'.toByte) == 0) Done(acc toString, s)
+            else {
+              val acc1 = acc.appendAll(e map (_ toChar))
+              val i = acc1.indexOf("\n\n")
+              if (i == -1) Cont(step(acc1))
+              else {
+                val res = acc1.toString
+                val left = res.slice(0, i+1)
+                val right = res.slice(i+1, res.length)
+                Done(left, El(right.toArray map (_ toByte)))
+              }
+            }
+          }
+        , empty = Cont(step(acc))
+        , eof = Done(acc toString, EOF[Chunk])
+        )
+      }
+      Cont(step(new StringBuilder))
+    }
+
+    def newline: IterV[Chunk,String] = {
+      def step(s: Input[Chunk]): IterV[Chunk,String] = {
+        s(el = e => {
+            if (e.indexOf('\n'.toByte) == 0) Done("\n", El(e tail))
+            else Done("", s)
+          }
+        , empty = Cont(step)
+        , eof = Done("", EOF[Chunk])
+        )
+      }
+      Cont(step)
     }
   }
 }
--- src/test/scala/fop/http/IterateesSpec.scala
+++ src/test/scala/fop/http/IterateesSpec.scala
@@ -7,28 +7,33 @@ import Prop.forAll
 import java.io.InputStream
 
 object IterateesSpecification extends Properties("Iteratees") {
+  import java.io.ByteArrayInputStream
   /**
-   * tests a raw http response, which really just means how the status line,
+   * tests an http response, which really just means how the status line,
    * headers, and body are organized in the input stream. this is not testing
    * the actual content within each segment
    */
-  property("raw http response structure") = forAll(
-    Gen.alphaStr suchThat (!_.contains("\n"))
-    , Gen.alphaStr suchThat (!_.contains("\n\n"))
-    , Gen.alphaStr
-    , Gen.choose(1,10)
-  ) { (a,b,c,d) =>
-    import java.io.ByteArrayInputStream
-    val input = new ByteArrayInputStream((a+"\n"+b+"\n\n"+c).toArray map (_.toByte))
-    io { input }.bracket((in: InputStream) => io { in.close }) { 
-      Enumerators.byteStream(_)(new Array[Byte](d))(Iteratees.httpResponse)
-    } unsafePerformIO match {
-      case IterV.Done(x,_) => { 
-        a == x._1 &&
-        b == x._2 &&
-        c == x._3
+  property("http response stream") = forAll(
+    Gen.alphaStr suchThat (!_.isEmpty), Gen.listOf(Gen.alphaStr suchThat (!_.isEmpty)), Gen.alphaStr, Gen.choose(1,10)) { 
+    (statusLine,headers,body,chunkSize) => {
+      // generic-message = start-line
+      //                   *(message-header CRLF)
+      //                   CRLF
+      //                   [ message-body ]
+      // start-line      = Request-Line | Status-Line
+      val headerStr = if (headers.length == 0) "" else headers.mkString("\n")+"\n"
+      val input = new ByteArrayInputStream((statusLine+"\n"+headerStr+"\n"+body).toArray map (_.toByte))
+      val setup = io { input }.bracket((in: InputStream) => io { in.close }) { 
+        Enumerators.byteStream(_)(new Array[Byte](chunkSize))(Iteratees.httpResponse)
+      } 
+      setup.unsafePerformIO match {
+        case IterV.Done(x,_) => { 
+          statusLine+"\n" == x._1 &&
+          headerStr == x._2 &&
+          body == x._3
+        }
+        case _ => false
       }
-      case _ => false
     }
   }
 }
